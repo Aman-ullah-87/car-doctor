6afecdd73ca43a496990c1b7aaf97a7ef5be4b6b
Aman-ullah-87 <khaman2645@gmail.com>
2023-05-12 22:44:13 +0600

    service details

--- src/Pages/Home/Service/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/Home/Service/ServiceDetails/ServiceDetails.jsx
@@ -22,8 +22,7 @@ const ServiceDetails = () => {
                     </>)}
                 </div>
                </div>
-               <div>
-                
+               <div>          
                </div>
 
             </div>
